de34689a4eb8f296db5bebe4856c106483d7088f
Deyvis Valdez <deyvis.vh@gmail.com>
2015-05-07 12:58:36 -0500

    Adding Authentication

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,6 @@
 class RecipesController < ApplicationController
   before_action :set_recipe, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
 
   # GET /recipes
   # GET /recipes.json
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150507034214) do
+ActiveRecord::Schema.define(version: 20150507170034) do
 
   create_table "cuisines", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -48,6 +48,24 @@ ActiveRecord::Schema.define(version: 20150507034214) do
   add_index "recipes", ["cuisine_id"], name: "index_recipes_on_cuisine_id", using: :btree
   add_index "recipes", ["food_preference_id"], name: "index_recipes_on_food_preference_id", using: :btree
   add_index "recipes", ["food_type_id"], name: "index_recipes_on_food_type_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "recipes", "cuisines"
   add_foreign_key "recipes", "food_preferences"
