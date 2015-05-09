219a9fda82cc77f4dac65668b8ac8a251571cdb1
Deyvis Valdez <deyvis.vh@gmail.com>
2015-05-09 14:05:50 -0500

    Validating recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,10 @@ class Recipe < ActiveRecord::Base
   belongs_to :food_type
   belongs_to :food_preference
   belongs_to :cuisine
+  validates :title, presence: true
+  validates :food_preference_id, presence: true
+  validates :food_type_id, presence: true
+  validates :cuisine_id, presence: true
 
   DIFFICULTY = %w(Easy Medium Hard)
 
